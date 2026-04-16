8a766eba8a4ad48da77bdb761c925697d69feaf0
wdevelper11-cloud <wdevelper11@gmail.com>
2026-04-16 22:52:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const API_BASE = window.NEXUS_API_BASE || 'https://nexus-backend1-1.onrender.com';
 
-const ADMIN_CLERK_ID = '[INSERT_YOUR_CLERK_ID_HERE]';
+const ADMIN_CLERK_ID = 'user_3COSZv72S5YK9yrCkO5BcNtmaxm';
 const VIEW = {
   GUEST: 'GUEST',
   ENTERPRISE: 'ENTERPRISE',
